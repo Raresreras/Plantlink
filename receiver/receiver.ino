03a1689fd95262763e3d29b4e5ca7070ad2a04c0
Raresreras <89092397+Raresreras@users.noreply.github.com>
2024-07-16 18:16:03 +0300

    Update receiver.ino
    Improved functionality

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,29 +1,58 @@
-#include <VirtualWire.h>              
+#include <VirtualWire.h>
+
+#define pump1 8 //
+#define pump2 7 //
+#define pump3 9 //
+
+#define hum_sen_1 A1 //fertilizer level
+#define hum_sen_2 A2 //main water level
+
+//communication
 byte message[VW_MAX_MESSAGE_LEN]; //buffer
 byte messageLength = VW_MAX_MESSAGE_LEN; //message size
 int received_data = 0;
 
-void setup()
-{
+//humidity sensor maximum and minimum values
+int hum_min1 = 150;
+int hum_max1 = 500; 
+int hum_min2 = 150;
+int hum_max2 = 500; 
+
+void setup() {
   Serial.begin(9600);
-  Serial.println("Ready...");
+  pinMode(pump1, OUTPUT);
+  pinMode(pump2, OUTPUT);
+  pinMode(pump3, OUTPUT);
   vw_set_rx_pin(5);
   vw_setup(2000);
   vw_rx_start();
 }
 
-void loop()
-{
-  if (vw_get_message(message, &messageLength)) // non-blocking
-  {
-    Serial.print("Input: ");
-    for (int i = 0; i < messageLength; i++)
-    {
-      //Serial.print(message[i]);
+void loop() {
+  Serial.println("WL1_" + String(100 - get_level1())); //Water level 1
+  Serial.println("WL2_" + String(100 - get_level2())); //Water level 2
+  Serial.println(get_data_transmitter());
+}
+
+int get_data_transmitter(){
+  if (vw_get_message(message, &messageLength)){
+    for (int i = 0; i < messageLength; i++){
       received_data = message[i];
     }
-    //Serial.println(received_data);
-    Serial.write(received_data);
-    Serial.println("Poate");
+    return received_data;
   }
+}
+
+int get_level1(){
+  int value = analogRead(hum_sen_1);
+  if (value > hum_max1) hum_max1 = value;
+  if (value < hum_min1) hum_min1 = value;
+  return (float) (value - hum_min1)/(hum_max1 - hum_min1) * 100; 
+}
+
+int get_level2(){
+  int value = analogRead(hum_sen_2);
+  if (value > hum_max2) hum_max2 = value;
+  if (value < hum_min2) hum_min2 = value;
+  return (float) (value - hum_min2)/(hum_max2 - hum_min2) * 100; 
 }
