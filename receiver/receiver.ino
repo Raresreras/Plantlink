751e66cd72a4b17e45aa8e1ad0b996e791de44da
Raresreras <89092397+Raresreras@users.noreply.github.com>
2024-07-18 18:04:13 +0300

    Update receiver.ino
    Added receiver capability to receive data from serial

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -18,6 +18,9 @@ int hum_max1 = 500;
 int hum_min2 = 150;
 int hum_max2 = 500; 
 
+
+int pump_activ = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(pump1, OUTPUT);
@@ -29,9 +32,35 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("WL1_" + String(100 - get_level1())); //Water level 1
-  Serial.println("WL2_" + String(100 - get_level2())); //Water level 2
-  Serial.println(get_data_transmitter());
+  //Serial.println("WL1_" + String(100 - get_level1())); //Water level 1
+  //Serial.println("WL2_" + String(100 - get_level2())); //Water level 2
+  //Serial.println(get_data_transmitter());
+  if (Serial.available() > 0) pump_activ = read_serial();
+  switch(pump_activ){
+    case 1:
+      digitalWrite(pump1, HIGH);
+      delay(50);
+      digitalWrite(pump1, LOW);
+      break;
+    case 2:
+      digitalWrite(pump2, HIGH);
+      delay(50);
+      digitalWrite(pump2, LOW);
+      break;
+    case 3:
+      digitalWrite(pump3, HIGH);
+      delay(50);
+      digitalWrite(pump3, LOW);
+      break;
+  }
+}
+
+int read_serial(){
+  byte input = 0;
+  if (Serial.available() > 0) {
+    input = Serial.read();
+    return input - 48; //48 is the ascii value for 0 
+  }
 }
 
 int get_data_transmitter(){
