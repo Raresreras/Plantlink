26751e17686d611c402b376aa5e040e46b65c4d3
Raresreras <89092397+Raresreras@users.noreply.github.com>
2024-07-23 13:17:32 +0300

    Update receiver.ino

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,8 +1,8 @@
 #include <VirtualWire.h>
 
-#define pump1 8 //
-#define pump2 7 //
-#define pump3 9 //
+#define pump1 8 //fertilizer pump
+#define pump2 7 //water source pump
+#define pump3 9 //main pump
 
 #define hum_sen_1 A1 //fertilizer level
 #define hum_sen_2 A2 //main water level
@@ -12,17 +12,17 @@ byte message[VW_MAX_MESSAGE_LEN]; //buffer
 byte messageLength = VW_MAX_MESSAGE_LEN; //message size
 int received_data = 0;
 
-//humidity sensor maximum and minimum values
-int hum_min1 = 150;
+//humidity sensor maximum and minimum values - calibration values
+int hum_min1 = 195;
 int hum_max1 = 500; 
-int hum_min2 = 150;
-int hum_max2 = 500; 
+int hum_min2 = 550; 
+int hum_max2 = 1000; 
 
 
 int pump_activ = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   pinMode(pump1, OUTPUT);
   pinMode(pump2, OUTPUT);
   pinMode(pump3, OUTPUT);
@@ -33,34 +33,33 @@ void setup() {
 
 void loop() {
   Serial.println("WL1_" + String(100 - get_level1())); //Water level 1
-  Serial.println("WL2_" + String(100 - get_level2())); //Water level 2
+  Serial.println("WL2_" + String(100 - get_level2())); //Water level 2, multipliying value due to tank size
   Serial.println(get_data_transmitter());
-  if (Serial.available() > 0) pump_activ = read_serial();
-  switch(pump_activ){
-    case 1:
-      digitalWrite(pump1, HIGH);
-      delay(50);
-      digitalWrite(pump1, LOW);
-      break;
-    case 2:
-      digitalWrite(pump2, HIGH);
-      delay(50);
-      digitalWrite(pump2, LOW);
-      break;
-    case 3:
-      digitalWrite(pump3, HIGH);
-      delay(50);
-      digitalWrite(pump3, LOW);
-      break;
-  }
-}
+  while (Serial.available() > 0) {
+    pump_activ = Serial.readStringUntil("/n").toInt();
+      if (pump_activ > 0 && pump_activ < 5){
+        delay(50);
 
-int read_serial(){
-  byte input = 0;
-  if (Serial.available() > 0) {
-    input = Serial.read();
-    return input - 48; //48 is the ascii value for 0 
-  }
+        if (pump_activ == 1){
+          digitalWrite(pump1, HIGH);
+          delay(2000);
+          digitalWrite(pump1, LOW);
+        }
+        if (pump_activ == 2){
+          digitalWrite(pump2, HIGH);
+          delay(2000);
+          digitalWrite(pump2, LOW);
+        }
+        if (pump_activ == 3){
+          digitalWrite(pump3, HIGH);
+          delay(2000);
+          digitalWrite(pump3, LOW);
+          pump_activ = 0;
+        }
+      }
+    
+    }
+
 }
 
 int get_data_transmitter(){
